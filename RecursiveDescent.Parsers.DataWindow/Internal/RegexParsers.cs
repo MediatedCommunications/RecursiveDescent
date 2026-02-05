162226951cdc97b1a33e8d5497bcc49cd3caae7c
TonyValenti <Tony.Valenti@GMail.com>
2026-02-05 17:01:43 -0600

    Convert GeneratedRegex from Methods to Properties

--- RecursiveDescent.Parsers.DataWindow/Internal/RegexParsers.cs
+++ RecursiveDescent.Parsers.DataWindow/Internal/RegexParsers.cs
@@ -2,61 +2,61 @@
 
 namespace RecursiveDescent.Parsers.DataWindow; 
 
-internal sealed partial class ParenOpenParser() : RegexParser<ParenOpenNode>(GetGeneratedRegex()) {
+internal sealed partial class ParenOpenParser() : RegexParser<ParenOpenNode>(GetGeneratedRegex) {
 
     [GeneratedRegex(@"^\(", RegexOptions.IgnoreCase)]
-    private static partial Regex GetGeneratedRegex();
+    private static partial Regex GetGeneratedRegex { get; }
 
     protected override ParenOpenNode CreateNode(string Value) {
         return new ParenOpenNode() { Value = Value, };
     }
 }
 
-internal sealed partial class ParenCloseParser() : RegexParser<ParenCloseNode>(GetGeneratedRegex()) {
+internal sealed partial class ParenCloseParser() : RegexParser<ParenCloseNode>(GetGeneratedRegex) {
 
     [GeneratedRegex(@"^\)", RegexOptions.IgnoreCase)]
-    private static partial Regex GetGeneratedRegex();
+    private static partial Regex GetGeneratedRegex { get; }
 
     protected override ParenCloseNode CreateNode(string Value) {
         return new ParenCloseNode() { Value = Value, };
     }
 }
 
-internal sealed partial class QuoteParser() : RegexParser<QuoteNode>(GetGeneratedRegex()) {
+internal sealed partial class QuoteParser() : RegexParser<QuoteNode>(GetGeneratedRegex) {
 
     [GeneratedRegex(@"^\""", RegexOptions.IgnoreCase)]
-    private static partial Regex GetGeneratedRegex();
+    private static partial Regex GetGeneratedRegex { get; }
 
     protected override QuoteNode CreateNode(string Value) {
         return new QuoteNode() { Value = Value, };
     }
 }
 
-internal sealed partial class ProducesParser() : RegexParser<ProducesNode>(GetGeneratedRegex()) {
+internal sealed partial class ProducesParser() : RegexParser<ProducesNode>(GetGeneratedRegex) {
 
     [GeneratedRegex(@"^=", RegexOptions.IgnoreCase)]
-    private static partial Regex GetGeneratedRegex();
+    private static partial Regex GetGeneratedRegex { get; }
 
     protected override ProducesNode CreateNode(string Value) {
         return new ProducesNode() { Value = Value, };
     }
 }
 
-internal sealed partial class ShortStringParser() : RegexParser<ShortStringNode>(GetGeneratedRegex()) {
+internal sealed partial class ShortStringParser() : RegexParser<ShortStringNode>(GetGeneratedRegex) {
 
     [GeneratedRegex(@"^(\w|\.)+", RegexOptions.IgnoreCase)]
-    private static partial Regex GetGeneratedRegex();
+    private static partial Regex GetGeneratedRegex { get; }
 
     protected override ShortStringNode CreateNode(string Value) {
         return new ShortStringNode() { Value = Value, };
     }
 }
 
-internal sealed partial class LongStringParser() : RegexParser<LongStringNode>(GetGeneratedRegex()) {
+internal sealed partial class LongStringParser() : RegexParser<LongStringNode>(GetGeneratedRegex) {
 
     //This shoud match anything except for the terminal quote \"
     [GeneratedRegex(@"^(.*?)(?=\"")", RegexOptions.IgnoreCase)]
-    private static partial Regex GetGeneratedRegex();
+    private static partial Regex GetGeneratedRegex { get; }
 
     protected override LongStringNode CreateNode(string Value) {
         return new LongStringNode() { Value = Value, };
--- RecursiveDescent.Parsers/WhiteSpaceParser.cs
+++ RecursiveDescent.Parsers/WhiteSpaceParser.cs
@@ -1,10 +1,10 @@
 ﻿using System.Text.RegularExpressions;
 
 namespace RecursiveDescent.Parsers; 
-public sealed partial class WhiteSpaceParser() : RegexParser<WhiteSpaceNode>(GetGeneratedRegex()) {
+public sealed partial class WhiteSpaceParser() : RegexParser<WhiteSpaceNode>(GetGeneratedRegex) {
 
     [GeneratedRegex(@"\s*", RegexOptions.IgnoreCase)]
-    private static partial Regex GetGeneratedRegex();
+    private static partial Regex GetGeneratedRegex { get; }
 
     protected override WhiteSpaceNode CreateNode(string Value) {
         return new WhiteSpaceNode() { Value = Value, };
